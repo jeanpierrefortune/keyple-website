99ee074a39e7d93f382b27d593ab908faaf67f0b
jeanpierrefortune <jeanpierrefortune@users.noreply.github.com>
2023-05-10 20:36:15 +0000

    deploy: f31740a20777cd7b49266de24a445181b7d9c775

--- .github/scripts/dashboard_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Initialize the project dashboard content..."
-
-token=$1
-
-../.github/scripts/dashboard_update_repos.sh $token
-
-echo "Local dashboard data initialisation finished."
--- content/learn/example-code/_index.md
+++
@@ -1,9 +0,0 @@
----
-title: Example code
-type: book
-toc: false
-weight: 5
----
-
----
-{{< list_children >}}
